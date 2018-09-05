541cd50788254fe354afd33a023468fac060f7e5
Iven Krall <iven.krall@bewotec.de>
2018-09-05 09:35:51 +0200

    Fixed missing reference

--- src/main/java/modern/reflare/util/Image.kt
+++ src/main/java/modern/reflare/util/Image.kt
@@ -5,8 +5,10 @@ import javax.imageio.ImageIO
 import javax.swing.ImageIcon
 
 internal fun getScaledIconResource(resource: String, width: Int, height: Int): ImageIcon {
-    val image = ImageIO.read(ImageHelper::class.java.getResourceAsStream(resource))
+    val image = ImageIO.read(Ref::class.java.getResourceAsStream(resource))
     val scaledImage = image.getScaledInstance(width, height, Image.SCALE_SMOOTH)
 
     return ImageIcon(scaledImage)
 }
+
+private class Ref
